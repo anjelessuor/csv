33c622d8850d54f07c821c6c56be85139dab4d4c
ClemenceChoupette <clemenceducornet@gmail.com>
2017-04-26 14:10:32 +0200

    activities - in progress

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,9 @@
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/security/login', 'Security#login', 'security_login'],
 		['GET|POST', '/security/register', 'Security#register', 'security_register'],
+		['GET|POST', '/activities/create', 'Activities#create', 'activities_create'],
+		['GET|POST', '/activities/index', 'Activities#index', 'activities_index'],
+		['GET|POST', '/activities/edit/[i:id]', 'Activities#edit', 'activities_edit'],
+		['GET|POST', '/activities/delete/[i:id]', 'Activities#delete', 'activities_delete'],
+		['GET', '/activities/[i:id]', 'Activities#view', 'activities_view'],
 	);
