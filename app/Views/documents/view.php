9ca6a6c076eb3b5da9da6895123642e5d0625010
Ambre Saulnier <ambresaulnier@live.fr>
2017-04-27 16:26:43 +0200

    commit - pour les documents à telecharger

--- app/Views/documents/view.php
+++ app/Views/documents/view.php
@@ -2,9 +2,9 @@
 
 <?php $this->start('main_content') ?>
     <article>
-        <p><?php echo $documents_name['documents_name'] ?></p>
-        <p><?php echo $documents_description['documnents_description'] ?></p>
-        <p><?php echo $documents_document = $_FILES["documents_document"]["name"] ?></p>
+        <p><?php echo $document['documents_name'] ?></p>
+        <p><?php echo $documents['documnents_description'] ?></p>
+        <p><?php echo $documents["documents_document"]["name"] ?></p>
     </article>
 
 
