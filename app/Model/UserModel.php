3c25b69086d7059a33aa16907b745271eb584da7
Ambre Saulnier <ambresaulnier@live.fr>
2017-04-28 15:35:08 +0200

    changes not finish

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -7,34 +7,34 @@ class UserModel extends UsersModel //On hérite de UserModel car il possède des
 {
     protected $primaryKey = 'user_id';
 
-    public function changeTokenLogin($user_id) {
-    $dbh = ConnectionModel::getDbh();
-    $token_login = sha1(md5(sha1($user_id) . sha1(time() b) . md5('1a4g51rz74hz21rz4h') . md5(uniqid()))); // Génére un token du style 3a4f74a7f5a7f4v7g4ae5g41ae2gea87gv
-    $this->dbh->query('UPDATE users SET token_login = "'.$token_login.'" WHERE user_id = ' . $user_id);
-    return $token_login;
-    }
-
-    function checkUserByEmail($user_email) {
-    $dbh = ConnectionModel::getDbh();
-    $query = $dbh->prepare('SELECT id_user FROM users WHERE user_email = :user_email');
-    $query->bindValue(':user_email', $user_email, PDO::PARAM_STR);
-    $query->execute();
-    return $query->fetchColumn();
-}
     function isValidToken($token_login) {
-    $dbh = ConnectionModel::getDbh();
     $query = $dbh->prepare('SELECT id_user FROM users WHERE token_forget = :token AND NOW() < date_forget');
     $query->bindValue(':token', $token, PDO::PARAM_STR);
     $query->execute();
     return $query->fetchColumn();
     }
 
+
     function changeUserPassword($user_id, $user_password) {
-    $dbh = ConnectionModel::getDbh();
     $query = $dbh->prepare('UPDATE users SET user_password = :user_password, token_forget = NULL, date_forget = NULL WHERE user_id = :user_id'); // On met à jour le mot de passe de l'utilisateur et on supprime le token
     $query->bindValue('user_id', $user_id);
     $query->bindValue('user_password', password_hash($user_password, PASSWORD_DEFAULT));
     return $query->execute();
     }
+
+
+    public function changeTokenLogin($user_id) {
+    $token_login = sha1(md5(sha1($user_id) . sha1(time() b) . md5('1a4g51rz74hz21rz4h') . md5(uniqid()))); // Génére un token du style 3a4f74a7f5a7f4v7g4ae5g41ae2gea87gv
+    $this->dbh->query('UPDATE users SET token_login = "'.$token_login.'" WHERE user_id = ' . $user_id);
+    return $token_login;
+    }
+
+    function checkUserByEmail($user_email) {
+    $query =$this->$dbh->prepare('SELECT id_user FROM users WHERE user_email = :user_email');
+    $query->bindValue(':user_email', $user_email, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchColumn();
+    }
+
 
 }
