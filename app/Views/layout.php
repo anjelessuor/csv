0d3d7525698b5c4f97a12bf210c226a319373e94
Schp4wn <schp4wn@gmail.com>
2017-05-04 03:57:56 +0200

    DesignUpdated 0.8
    implementing Paralax.js
    timer js
    smothscroll
    navbarchanges *workinprogress*

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -13,10 +13,10 @@
 
 <!--Navbar-->
 		<header class="cd-header">
-		<div class="cd-logo"><a href="#"><img src="./assets/img/cd-logo.svg" alt="Placeholder"></a></div>
+		<div class="cd-logo"><img src="./assets/img/cd-logo.svg" alt=""></a></div>
 		<nav>
 			<ul class="cd-secondary-nav">
-				<li><a href="<?php echo $this->url('default_home'); ?>">ACCUEIL</a></li>
+				<li><a class="smoothscroll" href="#home" href="<?php echo $this->url('default_home'); ?>">ACCUEIL</a></li>
 				<li><a href="<?php echo $this->url('section_qui-sommes-nous'); ?>">QUI SOMMES-NOUS ?</a></li>
 				<li><a href="<?php echo $this->url('section_enfance-3-5'); ?>">SECTEUR ENFANCE (3-5ans)</a></li>
 				<li><a href="<?php echo $this->url('section_enfance-6-12'); ?>">SECTEUR ENFANCE (6-12ans)</a></li>
@@ -28,10 +28,12 @@
 			</ul>
 		</nav> <!-- cd-nav -->
 
-		<a class="cd-primary-nav-trigger" href="#0">
-			<span class="cd-menu-text">Menu</span><span class="cd-menu-icon"></span>
+		<a class="cd-primary-nav-trigger" href="#">
+			<span class="cd-menu-text">Menu</span>
+			<span class="cd-menu-icon"></span>
 		</a> <!-- cd-primary-nav-trigger -->
 	</header>
+
 	<nav>
 		<ul class="cd-primary-nav">
 			<li class="cd-label">QUI SOMMES-NOUS ?</li>
@@ -51,49 +53,51 @@
 
 			<li><a href="#0">LoremIpsum</a></li>
 			<li><a href="#0">LoremIpsum</a></li>
-			<li class="cd-label">next Seperate</li>
-
-			<li><a href="#0">LoremIpsum</a></li>
-			<li><a href="#0">LoremIpsum</a></li>
-			<li class="cd-label">next Seperate</li>
-
-			<li><a href="#0">LoremIpsum</a></li>
-			<li><a href="#0">LoremIpsum</a></li>
 
 			<li class="cd-label">Follow us</li>
 			<li class="cd-social cd-facebook"><a href="#0">Facebook</a></li>
 		</ul>
 	</nav>
 <!--END NAVBAR-->
+<!--parallax effect  -->
+
 
 
 <!--Section field-->
 
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-xs-12">
-      <section>
+      <!-- <section> -->
         <?= $this->section('main_content') ?>
-      </section>
-    </div>
-  </div>
-</div>
+      <!-- </section> -->
+
 <!--Section field END-->
 
-
 <!--Footer-->
+
 <footer>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-6 col-md-12">
-				<p> Copyright 2017 - Rising Goone Squad </p>
-			</div>
-		</div>
-	</div>
-</footer>
+      <div class="container">
+
+            <ul class="social-links">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+            </ul>
+            <ul class="copyright">
+               <li>&copy; Copyright 2017 </li>
+               <li>Design by <a title="Styleshout" href="http://www.google.com/">Rising Goone Squad</a></li>
+            </ul>
+
+         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+      </div>
+   </footer> <!-- Footer End-->
 <!--Footer END-->
 
 <!-- SCRIPTS -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script type="text/javascript" src="<?= $this->assetUrl('js/parallax.min.js'); ?>"></script>
 	<script type="text/javascript" src="<?= $this->assetUrl('js/jQuery-3.1.1.min.js'); ?>"></script>
 	<script type="text/javascript" src="<?= $this->assetUrl('js/bootstrap.min.js'); ?>"></script>
 	<script type="text/javascript" src="<?= $this->assetUrl('js/app.js'); ?>"></script>
